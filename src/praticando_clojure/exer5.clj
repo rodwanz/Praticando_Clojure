1e5fca40bc03d885fde5f91dd46715568413b397
Rodrigo Wanzeler <wanzervai@gmail.com>
2025-01-06 17:02:31 -0300

    Filtrando mapas e criando composições com comp

--- src/praticando_clojure/exer5.clj
+++ src/praticando_clojure/exer5.clj
@@ -47,4 +47,40 @@
 (println (total-do-pedido pedido))
 
 
+(def pedido {:mochila {:quantidade 2, :preco 80}
+             :camiseta {:quantidade 3, :preco 40}
+             :chaveiro {:quantidade 1}})
+
+(defn gratuito? [item]
+  (<= (get item :preco 0) 0))
+(println "Fltrantando suvenir")
+(println (filter gratuito? (vals pedido)))
+
+
+(defn gratuito? [[chave item]]
+  (<= (get item :preco 0) 0))
+(println "Fltrantando suvenir")
+(println (filter gratuito? pedido))
+
+
+(defn gratuito? [item]
+  (<= (get item :preco 0) 0))
+(println "Fltrantando suvenir, usando função anonima")
+(println (filter (fn [[chave item]]  (gratuito? item)) pedido))
+(println "Fltrantando suvenir, usando lambda")
+(println (filter #(gratuito? (second %)) pedido))
+
+
+(defn pago? [item]
+  (not (gratuito? item)))
+(println (pago? {:preco 50}))
+(println (pago? {:preco 0}))
+(println ((comp not gratuito?) {:preco 50}))
+
+
+(def pago? (comp not gratuito?))
+(println (pago? {:preco 50}))
+(println (pago? {:preco 0}))
+
+
 
